f1638716731a7b6cb77cdbe374321f447f4e3dbe
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2018-08-21 23:43:08 +0000

    Created post if statement

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const http = require('http');
 const fs  = require('fs');
 const path = require('path');
+// const data = require('./data/products');
+const qs = require('querystring');
 
 
 // ─── SERVER START ───────────────────────────────────────────────────────────────
@@ -8,13 +10,13 @@ var server = http.createServer(function(request, response){
     console.log(`${request.method} request for ${request.url}`);
 
     
-    // ─── GET IF STATEMENT ───────────────────────────────────────────────────────────
+    // ─── GET ───────────────────────────────────────────────────────────
     //
     if (request.method === 'GET'){
 
         
         // index request
-        if(request.url === "/" || request.url === '/home' || request.url === '/index' || request.url === '/index.html'){
+        if(request.url === '/' || request.url === '/home' || request.url === '/index' || request.url === '/index.html'){
             fs.readFile('./public/index.html', 'UTF-8', function(error, contents){
                 if (error) {
                     console.log('error');
@@ -46,7 +48,53 @@ var server = http.createServer(function(request, response){
         
     }
     //
-    // ───────────────────────────────────────────────────── GET IF STATEMENT END ─────
+    // ───────────────────────────────────────────────────── GET  END ─────
+
+
+    //
+    // ─── POST ───────────────────────────────────────────────────────────────────────
+    //
+    else if (request.method === 'POST') {
+        
+        
+        if (request.url === '/submitForm') {
+            var body = '';
+            request.on('data', function(data){
+                body += data;
+            })
+
+            request.on('end', function(){
+
+            });
+            console.log('working');
+
+            request.on('end', function(){
+                var formData = qs.parse(body);
+                console.log(formData);
+                response.writeHead(302, {
+                    'Location': '/'
+                });
+                response.end();
+            });
+            
+            // var content = 'some text'
+            // fs.appendFile('./data/items.json', content, function(err){
+            //     if (err){
+            //         console.log('error in appending data')
+            //     } else {
+            //         console.log('content was appended');
+            //         console.log(content.value)
+            //     }
+            // });
+        }
+    }
+    //
+    // ───────────────────────────────────────────────────────────────── POST END ─────
+    //
+
+        
+
+        
 
         
     
